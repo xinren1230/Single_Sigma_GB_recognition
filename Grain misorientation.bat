1ddf09d4980021052a4e9a0dd12a7bc58db37271
x.chen <x.chen@mpie.de>
2025-05-12 16:39:52 +0200

    1: Add launch.bat

--- Grain misorientation.bat
+++ Grain misorientation.bat
@@ -1,11 +1,5 @@
 @echo off
-
-CALL C:\Users\x.chen\Anaconda3\Scripts\activate.bat C:\Users\x.chen\Anaconda3
-
-CALL conda activate base
-
-CD /D "C:\Users\x.chen\Single_Sigma_GB_recognition"
-
-python "Grain_misorientation.py"
+call gb_env\Scripts\activate
+python Grain_misorientation.py
 
 pause
